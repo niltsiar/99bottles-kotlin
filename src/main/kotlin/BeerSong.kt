1725c7e0180058fc35d2df58fe28d52c6fb1f6b1
Borja Quevedo <borjota@gmail.com>
2018-09-15 12:39:50 +0200

    Before 1-six pack

--- src/main/kotlin/BeerSong.kt
+++ src/main/kotlin/BeerSong.kt
@@ -2,6 +2,21 @@ class BeerSong {
 
     companion object {
 
-        fun verses(from: Int, to: Int): String = ""
+        fun verses(from: Int, to: Int): String = (from downTo to)
+            .map {
+                when (it) {
+                    in 99 downTo 3 -> "$it bottles of beer on the wall, $it bottles of beer.\nTake one down and pass it around, ${(it - 1)} bottles of beer on the wall.\n"
+                    2 -> "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+                    1 -> "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+                    0 -> "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+                    else -> throw IllegalArgumentException()
+                }
+            }.reduce { acc, s -> acc + s }
     }
+}
+
+fun Int.toBottles() = when (this) {
+    in 99 downTo 2 -> "$this bottles"
+    1 -> "1 bottle"
+    else -> throw IllegalArgumentException()
 }
